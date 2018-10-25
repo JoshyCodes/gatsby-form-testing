cefb570c1ba50bd14a80b4ed62981b38836eee77
Joshua Fischer <jfischeronline@gmail.com>
2018-10-25 16:33:58 -0400

    changed form

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,17 +8,26 @@ const IndexPage = () => (
   <Layout style="text-align: center;">
 
 
-      <form className="m-form" name="helpdesk" method="POST" netlify-honeypot="bot-field" data-netlify={true}>
-
-      <input name="bot-field" hidden></input>
-        <br />
-      <input placeholder="Your Name" required></input>
-        <br />
-      <textarea placeholder="What can we help you with?" required></textarea>
-        <br />    
-      <button type="submit">Submit Ticket</button>
-
-      </form>
+<form name="contact" method="POST" netlify>
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
 
 
   </Layout>
