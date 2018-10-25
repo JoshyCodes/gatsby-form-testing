49503456b5540056cda5efca1886726dd02b4f8a
Joshua Fischer <jfischeronline@gmail.com>
2018-10-25 16:27:52 -0400

    added input field

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,17 @@ import Layout from '../components/layout'
 import Image from '../components/image'
 
 const IndexPage = () => (
-  <Layout>
+  <Layout style="text-align: center;">
 
 
       <form className="m-form" name="helpdesk" method="POST" netlify-honeypot="bot-field" data-netlify={true}>
 
-      <input name="bot-field" hidden/>
-      
+      <input name="bot-field" hidden></input>
+
       <input placeholder="Your Name" required></input>
+        <br />>
       <textarea placeholder="What can we help you with?" required></textarea>
+        <br />>      
       <button type="submit">Submit Ticket</button>
 
       </form>
