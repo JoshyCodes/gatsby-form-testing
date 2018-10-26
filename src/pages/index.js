06f286aab834ed4902eb75c4d188dd82b547f0a8
Joshua Fischer <jfischeronline@gmail.com>
2018-10-25 20:49:44 -0400

    testing a copy pasta

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,10 @@ const IndexPage = () => (
 
   <Layout>
 
-    <form 
-      name="contact"
-      method="post" 
-      action="/"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-    >
-      <input type="hidden" name="form-name" value="contact" />
+<form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+  {/* You still need to add the hidden input with the form name to your JSX form */}    
+  <input type="hidden" name="form-name" value="contact" /> 
+  
       <p>
         <input name="name" />
       </p>
