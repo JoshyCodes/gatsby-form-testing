7fb2ebfa96f1bd69b58b93bb2749b23c20e71db1
Joshua Fischer <jfischeronline@gmail.com>
2018-11-01 14:08:08 -0400

    changes to the hidden...

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,22 +2,25 @@ import React from "react";
 
 const IndexPage = () => (
   <div>
+
     <h1>Greetings Friend!</h1>
     <p>Welcome to our new HelpDesk site.</p>
     <p>Please fill out the form below to begin the healing!</p>
+
     <form
       name="contact-form"
       method="post"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
       action="/thanks/"
-      hidden
     >
+      <input type="hidden" name="form-name" value="contact-form" />
       <input name="name" placeholder="Your Name" type="text" />
       <input name="email" placeholder="name@name.com" type="email" />
       <textarea name="message" />
       <button>Send</button>
     </form>
+
   </div>
 )
 
