72cfa7d7d607b8d10d6e9f6f65e5c479917e34ce
Joshua Fischer <jfischeronline@gmail.com>
2018-11-01 10:04:27 -0400

    redone contact as comp, with state handler.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,27 +2,14 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
+import ContactForm from '../components/contact'
 import Image from '../components/image'
 
 const IndexPage = () => (
 
   <Layout>
 
-    <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-
-      <input type="hidden" name="form-name" value="contact" /> 
-  
-      <p>
-        <input name="name" />
-      </p>
-      <p>
-        <textarea name="message" />
-      </p>
-      <p>
-        <button>Send</button>
-      </p>
-
-    </form>
+    <ContactForm />
 
   </Layout>
 
