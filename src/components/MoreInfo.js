2609fe7a7ee903c491dd98ccad0adc23849d91ed
Joshua Fischer <jfischeronline@gmail.com>
2018-11-08 15:14:15 -0500

    asd

--- src/components/MoreInfo.js
+++ src/components/MoreInfo.js
@@ -4,17 +4,12 @@ import Layout from '../components/layout'
 var helpdesk = require('../components/helpdesk')
 
 const MoreInfo = () => (
-
-  <Layout>
     
     <div className="c-intro">
       <h1 className="c-intro__title">Greetings Friend!</h1>
       <p>This is page two.</p>
     </div>
-
  
-  </Layout>
-
 )
 
 export default MoreInfo
