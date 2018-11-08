f50224b7c9b6a15ebc222415a3ab4908bcf039c0
Joshua Fischer <jfischeronline@gmail.com>
2018-11-08 13:57:09 -0500

    stuff again

--- src/components/router.js
+++
@@ -1,9 +0,0 @@
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-
-
-
-    <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={} />
-        </Switch>
-    </BrowserRouter>
